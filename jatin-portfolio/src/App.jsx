54bcd0777ba235caf6bd6b599a682db39e8184f3
Jatin65483 <158288728+Jatin65483@users.noreply.github.com>
2024-10-31 12:01:11 +0530

    Router

--- jatin-portfolio/src/App.jsx
+++ jatin-portfolio/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Home from './Pages/Home'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
+  const router = BrowserRouter()
   return (
-    <div>
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="header" element={<Header />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
