85905c2ebf7378b644600f54d613eea1972f281a
Jatin65483 <jatinsharma65483@gmail.com>
2024-11-01 11:28:51 +0530

    j

--- jatin-portfolio/src/components/ProjectsSection/ProjectsSection.jsx
+++ jatin-portfolio/src/components/ProjectsSection/ProjectsSection.jsx
@@ -29,7 +29,7 @@ function ProjectsSection() {
             <div className='flex justify-between' style={{width:'100%'}}>
                 <div className='project-details'>
                     <div className='project-name'>Take a Trip</div>
-                    <div className='project-link'> <Link to='https://elitetailors.in'>elitetailors.in</Link></div>
+                    <div className='project-link'> <a href='https://elitetailors.in'>elitetailors.in</a></div>
                 </div>
                 <div className='project-role'>Full Stack Developer</div>
             </div>
@@ -40,7 +40,7 @@ function ProjectsSection() {
             <div className='flex justify-between' style={{width:'100%'}}>
                 <div className='project-details'>
                     <div className='project-name'>Project Name</div>
-                    <div className='project-link'>Project Repo Link</div>
+                    <div className='project-link'><a href='www.google.com'>google</a></div>
                 </div>
                 <div className='project-role'>Role</div>
             </div>
